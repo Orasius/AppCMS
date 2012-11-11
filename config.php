ab42684ead0ab147d0d5e3cc157203e89655d434
Oras Al-Kubaisi <oras@shloud.com>
2012-11-11 17:02:32 +0000

    changing values in config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@ include "classes/DB.php";
 $DBC = new DB();
 $DBC->host = "localhost";
 $DBC->username = "root";
-$DBC->password = "!@#ORAS!@#2010";
-$DBC->database = "appcms";
+$DBC->password = "";
+$DBC->database = "";
 
 $DBC->connect();
 
